a687e43119bf08f99a27406445fdcb0ad395d1d9
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-24 01:35:01 +0200

    Added fillable and migrations for collaborations

--- backend/app/Models/File.php
+++ backend/app/Models/File.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 
+        'name',    
+        'language',
+        'content',
+    ];
 }
