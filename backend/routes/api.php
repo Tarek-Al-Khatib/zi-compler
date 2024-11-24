f36635dbe0094e33ed092d3b0a7bffbfe5cb487c
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-24 17:34:51 +0200

    Added authentication routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,3 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::group([
+
+  'middleware' => 'api',
+  'prefix' => 'auth'
+
+], function ($router) {
+
+  Route::post('login', 'AuthController@login');
+  Route::post('logout', 'AuthController@logout');
+  Route::post('refresh', 'AuthController@refresh');
+  Route::post('me', 'AuthController@me');
+
+});
