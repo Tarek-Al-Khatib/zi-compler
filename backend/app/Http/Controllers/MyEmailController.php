9ecd7eb6ef6277a186a989d1eb86cece8f46ac9e
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-24 06:41:59 +0200

    added more functions to the email

--- backend/app/Http/Controllers/MyEmailController.php
+++ backend/app/Http/Controllers/MyEmailController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\MyEmail;
 
 class MyEmailController extends Controller{
 
-    function send(){
-        
+    public function sendCollaborationRequest($fileId, $receiverEmail)
+    {
+        // Get sender info (loggedin)
+        $sender = auth()->user();
+    
+        // receiverinfo
+        $receiver = User::where('email', $receiverEmail)->first();
+    
+        if (!$receiver) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    
+        // Create a URL for the collaboration request page 
+        $url = route('collaborations.accept', ['fileId' => $fileId, 'userId' => $receiver->id]);
+    
+        // Send email
+        Mail::to($receiver->email)->send(new CollaborationRequestMail($sender->name, $url));
+    
+        return response()->json(['message' => 'Collaboration request sent successfully']);
     }
 }
