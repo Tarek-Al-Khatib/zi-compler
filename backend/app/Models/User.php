e295cc6e15e6d7f94da7074dfbdc16a637c605af
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-24 17:32:35 +0200

    Configured User model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -1,10 +1,32 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 
-class User extends Model
+class User extends Authenticatable implements JWTSubject
 {
+    use HasApiTokens, HasFactory, Notifiable;
     protected $fillable = ["name", "email", "password"];
+
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
