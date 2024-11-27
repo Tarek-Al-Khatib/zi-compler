35ff573c9a578cc52e120d8f920e8bd138ea743e
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-27 18:17:09 +0200

    Fixed code layout on applayout

--- frontend/src/pages/Auth/AppLayout.jsx
+++ frontend/src/pages/Auth/AppLayout.jsx
@@ -1,40 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../../styles/leftPannel.css"
-import { useNavigate } from 'react-router-dom';
-import Navbar from '../../base/navbar';
+import "../../styles/leftPannel.css";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../../base/navbar";
 import LeftPannel from "../../components/LeftPanel";
 import RightPanel from "../../components/RightPanel";
 import CodeEditor from "../../components/CodeEditor";
 
-const AppLayout = () =>{
-
-
-
-
-
-    return(
-
-     <div className="layouts">
-        <Navbar/>
-        <div className="container">
-                
+const AppLayout = () => {
+  return (
+    <div className="layouts">
+      <Navbar />
+      <div className="container">
         <div className="left-panel">
-            <LeftPannel />
-            </div>
+          <LeftPannel />
+        </div>
 
-            <div className="middle-panel">
-                <CodeEditor/>
-            </div>
+        <div className="middle-panel">
+          <CodeEditor />
+        </div>
 
-      <div className="right-panel">
-       <RightPanel/>
+        <div className="right-panel">
+          <RightPanel />
+        </div>
       </div>
-            
-            </div>
-            </div>
-
-    );
-}
+    </div>
+  );
+};
 
 export default AppLayout;
